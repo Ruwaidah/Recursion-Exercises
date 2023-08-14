d3d2da761e1bee38aeb46548b12def671e38fecc
Ruwaidah Alfakhri <Ruwaidah.riyadh@gmail.com>
2023-08-13 19:53:45 -0500

    solve all the exercises with the further study

--- recursion.js
+++ recursion.js
@@ -28,7 +28,7 @@ function everyOther(str) {
 /** isPalindrome: checks whether a string is a palindrome or not. */
 // "tacocat"
 function isPalindrome(str) {
-  if (str.length === 2 || str.length === 3)
+  if (str.length <= 3)
     return str[0] === str[str.length - 1];
   return isPalindrome(str.slice(1, str.length - 1));
 }
@@ -52,12 +52,40 @@ function revString(str) {
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
-function gatherStrings(obj) {}
+function gatherStrings(obj, arr = []) {
+  for (key in obj) {
+    const value = obj[key];
+    if (typeof value === "string") arr.push(value);
+    else if (typeof value === "object") {
+      gatherStrings(obj[key], arr);
+    }
+  }
+  return arr;
+}
 
 /** binarySearch: given a sorted array of numbers, and a value,
  * return the index of that value (or -1 if val is not present). */
 
-function binarySearch(arr, val) {}
+function binarySearch(arr, val, midIndx=0) {
+  let mid 
+  if (arr.length === 1 ) {
+    mid = 0
+  } else {
+    mid = Math.floor(arr.length/2)
+  }
+
+  if (arr[mid] === val) {
+    if (midIndx > 0) return mid+midIndx+1
+    else return mid
+  }
+   if (val> arr[mid]  ) {
+   return binarySearch(arr.slice(mid+1), val, mid+midIndx)
+  }
+   if (val < arr[mid]) {
+   return binarySearch(arr.slice(0, mid), val)
+  }
+  return -1
+  }
 let animals = ["duck", "cat", "pony"];
 
 console.log(product([2, 3, 4]));
@@ -69,6 +97,34 @@ console.log(isPalindrome("tacodog"));
 console.log(revString("porcupine"));
 console.log(findIndex(animals, "cat")); // 1
 console.log(findIndex(animals, "porcupine")); // -1
+
+let nestedObj = {
+  firstName: "Lester",
+  favoriteNumber: 22,
+  moreData: {
+    lastName: "Testowitz",
+  },
+  funFacts: {
+    moreStuff: {
+      anotherNumber: 100,
+      deeplyNestedString: {
+        almostThere: {
+          success: "you made it!",
+        },
+      },
+    },
+    favoriteString: "nice!",
+  },
+};
+
+console.log(gatherStrings(nestedObj)); // ["Lester", "Testowitz", "you made it!", "nice!"];
+
+console.log(binarySearch([1,2,3,4],1)) // 0
+console.log(binarySearch([1,2,3,4],3)) // 2
+console.log(binarySearch([1,2,3,4],5)) // -1
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 6)) // 5
+
+
 
 module.exports = {
   product,
